2ff30711592a5fae9069c7fe23cc0d7afd52f6da
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-07-10 00:25:29 -0300

    Adicioando font Baloo Paaji 2

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,6 +8,7 @@
         <title>GestCondo</title>
 
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;500;600;700&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
