8acc659a465bc2a110e8e00efbb98a63cbf9a483
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-07-21 19:50:55 -0300

    Feat: Criado middleware para usuário support

---
+++ app/Http/Middleware/EnsureUserIsSupport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsSupport
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->user() || $request->user()->role != 'support') {
+            return response()->json(['message' => 'Acesso não autorizado.'], 403);
+        }
+
+        return $next($request);
+    }
+}
--- routes/condominium/index.php
+++ routes/condominium/index.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\CondominiumController;
+use App\Http\Middleware\EnsureUserIsSupport;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/condominium', [CondominiumController::class, 'store']);
-Route::get('/condominium', [CondominiumController::class, 'index']);
-Route::put('/condominium/{id}', [CondominiumController::class, 'update']);
+Route::middleware(['auth:sanctum', EnsureUserIsSupport::class])->group(function() {
+    Route::post('/condominium', [CondominiumController::class, 'store']);
+    Route::get('/condominium', [CondominiumController::class, 'index']);
+    Route::put('/condominium/{id}', [CondominiumController::class, 'update']);
+});
