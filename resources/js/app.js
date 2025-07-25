2ae4845137ee23cb1965458349c82aabb4122ec0
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-07-24 21:24:18 -0300

    Add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,16 @@ import ToastService from 'primevue/toastservice';
 import ConfirmationService from 'primevue/confirmationservice';
 import 'primeicons/primeicons.css';
 import { initTheme } from './helpers/theme';
+import { createPinia } from 'pinia';
 
 const appLoading = createApp(AppLoading);
 appLoading.mount('#appLoading');
 
 const app = createApp(App);
 app.use(router);
+
+const pinia = createPinia();
+app.use(pinia);
 
 app.use(PrimeVue, {
     theme: {
