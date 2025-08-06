914a5becf81f72240f8ca1f11fcdd1240e9702f4
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-08-05 23:36:10 -0300

    Fix na tabela de user_permissuins, add on cascade delete

--- database/migrations/2025_08_04_221720_create_user_permissions_table.php
+++ database/migrations/2025_08_04_221720_create_user_permissions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->unique();
+            $table->foreignId('user_id')->constrained('users')->unique()->onDelete('cascade');
             $table->json('permissions')->nullable();
             $table->timestamps();
         });
