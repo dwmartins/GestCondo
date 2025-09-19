25ade2c7438ec5b183a933c116c653da63d29f75
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-09-18 23:48:31 -0300

    feat: adicionado valdiação de logout extra

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { storeToRefs } from 'pinia';
 import { useCondominiumStore } from './stores/condominiumStore';
+import authService from './services/auth.service';
+import router from './router';
 
 // Interceptor de requisição
 axios.interceptors.request.use(config => {
@@ -13,3 +15,17 @@ axios.interceptors.request.use(config => {
 
     return config;
 });
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401 &&
+            error.response?.data?.message === 'Unauthenticated.') {
+            
+            authService.clearAuth();
+            router.push({ name: 'login' });
+        }
+
+        return Promise.reject(error);
+    }
+);
